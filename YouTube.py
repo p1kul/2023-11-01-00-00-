2a076ce9c218eb4a14c0bff71d8f31918ec78f0d
p1kul <funikova@me.com>
2024-07-10 14:08:25 +0300

    Add files via upload

--- YouTube.py
+++ YouTube.py
@@ -1,7 +1,7 @@
 import time
 
 class User:
-    def __init__(self, nickname  , password  , age ):
+    def __init__(self, nickname, password, age ):
         self.nickname = nickname
         self.password = password
         self.age = age
@@ -17,69 +17,79 @@ class User:
 
 
 class Video:
-    def __init__(self, title , duration , time_now : int = 0 , adult_mode : bool = False):
+    def __init__(self, title, duration,  adult_mode = False):
         self.title = title
         self.duration = duration
-        self.time_now = time_now
+        self.time_now = 0
         self.adult_mode = adult_mode
 
+    def __eq__(self, other):
+        return self.title.lower() == other.title.lower()
+
 class UrTube:
-    users = []
-    videos = []
-    current_user = None
+    def __init__(self):
+        self.users = []
+        self.videos = []
+        self.current_user = None
 
     def log_in(self, nickname , password):
         for user in self.users:
             if nickname == user.nickname and password == user.password:
                 self.current_user = user
 
-    def register(self, nickname , password , age):
-        for user in self.users:
-            if nickname  in user.nickname:
-                print(f'Пользователь {nickname} уже существует')
-                break
-
+    def register(self, nickname, password, age):
+        user_new = User(nickname, password, age)
+        if  user_new in self.users:
+            print(f'Пользователь {nickname} уже существует')
         else:
-            user = User(nickname, password, age)
-            self.users.append(user)
-            self.log_out()
-            self.log_in(user.nickname , user.password)
+            self.users.append(user_new)
+            self.current_user = user_new
 
     def log_out(self):
         self.current_user = None
 
     def add(self, *args):
         for video in args:
-            self.videos.append(video)
+            if video not in self.videos:
+                self.videos.append(video)
+            else:
+                print(f'Видео "{video.title}" уже существует.')
 
-    def get_videos(self , txt ):
+    def get_videos(self, txt):
         spisok_videos = []
         for video in self.videos:
             if txt.upper() in video.title.upper():
                 spisok_videos.append(video.title)
         return spisok_videos
 
-    def watch_video(self , video):
-        if self.current_user and self.current_user.age < 18 :
-            print('Вам нет 18 лет, пожалуйста покиньте страницу')
-        elif self.current_user:
-            for i in self.videos:
-                if video in i.title:
-                    for j in range(1,11):
-                        print(j, end=' ')
-                        time.sleep(1)
-                    print('Конец видео')
-        else:
-            print('Войдите в аккаунт, чтобы смотреть видео')
 
-    def __str__(self):
-        return f"{self.videos}"
+    def watch_video(self, title):
+        if not self.current_user:
+            print("Войдите в аккаунт, чтобы смотреть видео")
+            return
+
+        for video in self.videos:
+            if video.title == title:
+                if video.adult_mode and self.current_user.age < 18:
+                    print("Вам нет 18 лет, пожалуйста покиньте страницу")
+                    return
+
+                print(f"Начинается воспроизведение видео: {video.title}")
+                for second in range(video.time_now + 1, video.duration + 1):
+                    print(second, end=' ')
+                    time.sleep(1)
+                print("Конец видео")
+                video.time_now = 0
+                return
+
+
 
 
 if __name__ == '__main__':
     ur = UrTube()
     v1 = Video('Лучший язык программирования 2024 года', 200)
     v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
 
     # Добавление видео
     ur.add(v1, v2)
